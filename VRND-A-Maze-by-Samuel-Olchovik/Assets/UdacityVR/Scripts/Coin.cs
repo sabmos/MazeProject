96e87604bfdff014177227c0b81b702bdda00293
sabmos <23609502+sabmos@users.noreply.github.com>
2018-02-19 23:10:03 +0800

    Coins
    Added coins, coin script, coin animation, coin poof, particle system, and sounds

--- VRND-A-Maze-by-Samuel-Olchovik/Assets/UdacityVR/Scripts/Coin.cs
+++ VRND-A-Maze-by-Samuel-Olchovik/Assets/UdacityVR/Scripts/Coin.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Coin : MonoBehaviour 
 {
     //Create a reference to the CoinPoofPrefab
+	public GameObject coin;
+	public GameObject coinPoof;
 
     public void OnCoinClicked() {
         // Instantiate the CoinPoof Prefab where this coin is located
+		Object.Instantiate(coinPoof, transform.position, Quaternion.Euler(-90f, 0f, 0f));
         // Make sure the poof animates vertically
         // Destroy this coin. Check the Unity documentation on how to use Destroy
-    }
+		Destroy (coin);
 
+
+    }
 }
